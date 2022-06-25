2f35cea2b8434b723964ae6b94618b36ac70c095
Áureo Rodrigues <94138315+Aurinho007@users.noreply.github.com>
2022-06-25 08:34:26 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.getElementById("continua-pic").addEventListener('click', () => {
-    document.write("<img src='./images/milhoes.jpg'  id='continua-pic'/><img src='./images/milhoes.jpg'  id='continua-pic'/><img src='./images/milhoes.jpg'  id='continua-pic'/><img src='./images/milhoes.jpg'  id='continua-pic'/> <img src='./images/milhoes.jpg'  id='continua-pic'/> <img src='./images/milhoes.jpg'  id='continua-pic'/> <style> *{margin:0} #continua-pic{width:100%}</style>")
+    document.write("<img src='./images/milhoes2.jpg'  id='continua-pic'/><style> *{margin:0} #continua-pic{width:100%}</style>")
 
 })
 
