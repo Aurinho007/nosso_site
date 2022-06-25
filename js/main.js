80e281a195b6d865579e1df2c8add17023d749e1
aurinho <aurinhorodrigues@gmail.com>
2022-06-24 23:17:25 -0300

    egg

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
+document.getElementById("continua-pic").addEventListener('click', () => {
+    document.write("<img src='./images/milhoes.jpg'  id='continua-pic'/><img src='./images/milhoes.jpg'  id='continua-pic'/><img src='./images/milhoes.jpg'  id='continua-pic'/><img src='./images/milhoes.jpg'  id='continua-pic'/> <img src='./images/milhoes.jpg'  id='continua-pic'/> <img src='./images/milhoes.jpg'  id='continua-pic'/> <style> *{margin:0} #continua-pic{width:100%}</style>")
 
+})
 
