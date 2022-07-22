1ef40e6683df8c2883d90af3b7f490959ed35cae
Áureo Rodrigues <94138315+Aurinho007@users.noreply.github.com>
2022-07-22 13:19:40 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.getElementById("continua-pic").addEventListener('click', () => {
+    window.navigator.vibrate(500)
     document.write("<img src='./images/milhoes2.jpg'  id='continua-pic'/><style> *{margin:0} #continua-pic{width:100%}</style>")
 
 })
